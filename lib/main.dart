277fde55c7ef86ac4dcbf9278c22a69d38df0a75
yashas-hm <yashashm@gmail.com>
2021-04-13 23:03:58 +0530

    done app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_app/screens/product_overview_screen.dart';
+import './screens/product_overview_screen.dart';
 
 import './providers/auth.dart';
 import './providers/cart.dart';
